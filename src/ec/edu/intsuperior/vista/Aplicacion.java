af3c89d80d1133a0e0cbe1ce88a7f8793cd1a1bf
ssolis84 <santiago.solis@intsuperior.edu.ec>
2022-01-26 21:27:35 -0500

    Esta es la segunda versión del Proyecto Sentencias de Control

--- src/ec/edu/intsuperior/vista/Aplicacion.java
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -5,13 +5,23 @@
  */
 package ec.edu.intsuperior.vista;
 
+import ec.edu.intsuperior.controlador.Pais;
+
 /**
  *
  * @author Santiago
  */
 public class Aplicacion {
     public static void main(String[] args) {
-        System.out.println("hola");
+        Pais pais1= new Pais("p001","Argentina");
+        Pais pais2= new Pais("p002","Brasil");
+        Pais pais3= new Pais("p002","Bolivia");
+        Pais pais4= new Pais("p003","Chile");
+        
+        //System.out.println(pais1.getCodigoPais()+" - "+pais1.getNombrePais());
+        System.out.println(pais2.toString());
+        
+        
     }
     
 }
